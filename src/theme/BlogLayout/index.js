c85ea0890ad050f5c0fe5d15da9b85061cc4d01f
Stefan Oderbolz <oderbolz@gmail.com>
2025-10-28 12:50:40 +0100

    Add rss feed header

---
+++ src/theme/BlogLayout/index.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import clsx from 'clsx';
+import Layout from '@theme/Layout';
+import BlogSidebar from '@theme/BlogSidebar';
+export default function BlogLayout(props) {
+  const {sidebar, toc, children, ...layoutProps} = props;
+  const hasSidebar = sidebar && sidebar.items.length > 0;
+  const rss_url = "/blog/rss.xml"; // don't se useBaseUrl() here since this URL must not be localized
+  return (
+    <Layout {...layoutProps}>
+      <div className="container margin-vert--lg">
+        <div className="row">
+          <BlogSidebar sidebar={sidebar} />
+          <main
+            className={clsx('col', {
+              'col--7': hasSidebar,
+              'col--9 col--offset-1': !hasSidebar,
+            })}>
+              <p>Abonniere den <strong><a href={rss_url}>RSS feed</a></strong> um über aktuelle Entwicklungen rund um OGD auf dem Laufenden zu sein.</p>
+            {children}
+          </main>
+          {toc && <div className="col col--2">{toc}</div>}
+        </div>
+      </div>
+    </Layout>
+  );
+}
