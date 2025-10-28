302ea9ed81bb2e995fec231a49d3638d78b7c47f
Stefan Oderbolz <oderbolz@gmail.com>
2025-10-28 12:54:42 +0100

    Add base URL

--- src/theme/BlogLayout/index.js
+++ src/theme/BlogLayout/index.js
@@ -2,10 +2,11 @@ import React from 'react';
 import clsx from 'clsx';
 import Layout from '@theme/Layout';
 import BlogSidebar from '@theme/BlogSidebar';
+import useBaseUrl from '@docusaurus/useBaseUrl';
 export default function BlogLayout(props) {
   const {sidebar, toc, children, ...layoutProps} = props;
   const hasSidebar = sidebar && sidebar.items.length > 0;
-  const rss_url = "/blog/rss.xml"; // don't se useBaseUrl() here since this URL must not be localized
+  const rss_url = useBaseUrl("/blog/rss.xml");
   return (
     <Layout {...layoutProps}>
       <div className="container margin-vert--lg">
