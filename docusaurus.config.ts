e1ea4548b20a87c13a6455ff725e684933543b2e
Stefan Oderbolz <oderbolz@gmail.com>
2026-02-05 15:45:31 +0100

    Update social card image in Docusaurus config

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -97,7 +97,7 @@ const config: Config = {
 
   themeConfig: {
     // Replace with your project's social card
-    image: "img/docusaurus-social-card.jpg",
+    image: "img/winterthur-social-image.png",
 
     colorMode: {
       defaultMode: "light",
